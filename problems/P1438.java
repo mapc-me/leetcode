85e9cfa6c53129727086954631a89813f535e1ab
annakuzmenko <annakuzmenko@yandex-team.ru>
2022-02-06 00:19:18 +0300

    init and solved tasks

---
+++ problems/P1438.java
@@ -0,0 +1,54 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class P1438 {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,1,1};
+        System.out.println(longestSubarray(arr, 0));
+    }
+
+    public static int longestSubarray(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        int l = 0;
+        int r = 0;
+        int curAnswer = 0;
+        int answer = 0;
+
+        while (l < nums.length) {
+            Integer currentNum = nums[l];
+
+            if (map.isEmpty()
+                    || (Math.abs(map.firstKey() - currentNum) <= limit
+                    && Math.abs(map.lastKey() - currentNum) <= limit)) {
+                curAnswer++;
+                if (answer < curAnswer) {
+                    answer = curAnswer;
+                }
+                if (map.containsKey(currentNum)) {
+                    map.put(currentNum, map.get(currentNum) + 1);
+                } else {
+                    map.put(currentNum, 1);
+                }
+                l++;
+            } else {
+                curAnswer--;
+                Integer prevNum = nums[r];
+                if (map.get(prevNum) > 1) {
+                    map.put(prevNum, map.get(prevNum) - 1);
+                } else {
+                    map.remove(prevNum);
+                }
+                r++;
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ status.md
@@ -0,0 +1,12 @@
+### Leetcode Status
+
+- 30.01.22 2/6/1
+- 06.02.22 2/7/1
+
+### Problems
+
+* Sliding window [P1438]
+
+
+### Notes
+Need to refresh memory about sorted queue, tree sets (maps) [P1438]
