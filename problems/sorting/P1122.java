6dca0e51df16310be4861abaf7bbb437df5947c6
annakuzmenko <annakuzmenko@yandex-team.ru>
2022-02-13 17:17:27 +0300

    second kind of problem 1122 solving

--- problems/sorting/P1122.java
+++ problems/sorting/P1122.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class P1122 {
@@ -14,7 +18,8 @@ public class P1122 {
         int[] arr1 = {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19};
         int[] arr2 = {2, 1, 4, 3, 9, 6};
 
-        System.out.println(Arrays.toString(relativeSortArray(arr1, arr2)));
+//        System.out.println(Arrays.toString(relativeSortArray(arr1, arr2)));
+        System.out.println(Arrays.toString(fasterRelativeSortArray(arr1, arr2)));
     }
 
     // O(N^2)
@@ -45,6 +50,38 @@ public class P1122 {
 
     // O(N) with hash map
     public static int[] fasterRelativeSortArray(int[] arr1, int[] arr2) {
-        return null;
+        Set<Integer> arr2Numbers = new LinkedHashSet<>();
+        Map<Integer, Integer> crossNumbersCounterMap = new HashMap<>();
+        List<Integer> disjointList = new ArrayList<>();
+
+        List<Integer> result = new ArrayList<>();
+        for (Integer number : arr2) {
+            arr2Numbers.add(number);
+        }
+
+        for (Integer number : arr1) {
+
+            if (arr2Numbers.contains(number)) {
+                if (crossNumbersCounterMap.containsKey(number)) {
+                    crossNumbersCounterMap.put(number, crossNumbersCounterMap.get(number) + 1);
+                } else {
+                    crossNumbersCounterMap.put(number, 1);
+                }
+            } else {
+                disjointList.add(number);
+
+            }
+        }
+
+        arr2Numbers.forEach(key -> {
+            Integer count = crossNumbersCounterMap.get(key);
+            for (int i = 0; i < count; i ++) {
+                result.add(key);
+            }
+        });
+        Collections.sort(disjointList);
+        result.addAll(disjointList);
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
