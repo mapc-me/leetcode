b5ee568ad1703bbfe0002f6137448fa02c4f3a8f
annakuzmenko <annakuzmenko@yandex-team.ru>
2022-02-20 00:37:13 +0300

    Revert "added new solutions"
    This reverts commit 809483f6

--- problems/slidingwindow/P239.java
+++ problems/slidingwindow/P239.java
@@ -1,9 +1,13 @@
 package slidingwindow;
 
+import com.sun.source.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 public class P239 {
