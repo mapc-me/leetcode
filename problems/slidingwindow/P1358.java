066e4c7b60ced101e70896cd23bf993683e5059b
mapc-me <mapc.me@yandex.ru>
2022-10-25 23:48:52 +0200

    siding window

---
+++ problems/slidingwindow/P1358.java
@@ -0,0 +1,33 @@
+package slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P1358 {
+    public int numberOfSubstrings(String s) {
+        int[] track = new int[3]; // 0 - 'a', 1 - 'b', 2 - 'c'
+
+        int lastOne = 0;
+        int ans = 0;
+
+        int l = 0;
+
+        for (int i = 0; i < s.length(); i ++) {
+            track[s.charAt(i) - 'a'] ++;
+
+            while (hasTriple(track)) {
+                lastOne += 1;
+                track[s.charAt(l) - 'a']--;
+                l ++;
+            }
+            ans += lastOne;
+        }
+
+        return ans;
+
+    }
+
+    public boolean hasTriple(int[] track) {
+        return track[0] > 0 && track[1] > 0 && track[2] > 0;
+    }
+}
